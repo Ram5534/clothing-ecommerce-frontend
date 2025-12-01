dc333fdd27be572178d60bbf24cacfc97cfbd0fd
Ram5534 <kurakularammohanrao@gmail.com>
2025-12-01 11:03:11 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,9 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // allow cookies for JWT
+  headers: {
+    "Cache-Control": "no-cache",
+  },
 });
 
 export default API;
